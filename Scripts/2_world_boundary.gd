54a792598427e61e8a1612592f1be0f054ff7789
PeterBTech <solidsword@icloud.com>
2025-11-07 12:16:18 -0500

    Added code comments

--- Scripts/2_world_boundary.gd
+++ Scripts/2_world_boundary.gd
@@ -4,9 +4,13 @@ extends Area2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
+	#plays hawk sound
 	animation_player.play("HawkCry")
 	timer.start()
 	
 	
 func _on_timer_timeout() -> void:
+	#reloads level
 	get_tree().reload_current_scene()
+
+s
