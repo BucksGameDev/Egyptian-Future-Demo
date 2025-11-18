e4f7f97d542e9ece4003438c089c4f51f44bac37
PeterBTech <solidsword@icloud.com>
2025-11-18 11:41:39 -0500

    Background change
    Added a feature that changes the background visibility based off if the player has entered the area

--- Scenes/tile_map.gd
+++ Scenes/tile_map.gd
@@ -1,11 +1,16 @@
 extends TileMap
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_layer_z_index(1,-2)
+	set_layer_z_index(2,-3)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("entered!!")
+	set_layer_z_index(2, -1)
