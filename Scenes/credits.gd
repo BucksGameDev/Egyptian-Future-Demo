114e66a682e0624008df2fea9671d7a2e38dc354
PeterBTech <solidsword@icloud.com>
2025-11-20 10:40:10 -0500

    Added credits to menu

--- Scenes/credits.gd
+++ Scenes/credits.gd
@@ -11,13 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Person1-Scene.tscn")
-
-
-func _on_options_pressed() -> void:
-	pass
-
-
-func _on_credits_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Credits.tscn")
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
