8d9e5b4072d68578ce420050f15044cbf982625b
PeterBTech <solidsword@icloud.com>
2025-11-20 10:11:51 -0500

    Start branch

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -13,3 +13,11 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Person1-Scene.tscn")
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
