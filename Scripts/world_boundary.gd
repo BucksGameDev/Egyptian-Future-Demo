4a7df00fba28cb15cbfadc16914d80996c89c2d7
PeterBTech <solidsword@icloud.com>
2025-11-21 20:43:43 -0500

    World Boundary Takes Hearts

--- Scripts/world_boundary.gd
+++ Scripts/world_boundary.gd
@@ -1,18 +1,27 @@
 extends Area2D
+@onready var timer_2: Timer = $Timer2
 
 @onready var timer: Timer = $Timer
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
 var player
-
-func _on_body_entered(body: Node2D) -> void:
-	#plays hawk sounds
-	animation_player.play("HawkCry")
+var falling
+func _ready() -> void:
+	falling = false
 	player = get_tree().current_scene.get_node("EgyptianGuy")
-	player._damage()
-	#timer.start()
+@onready var falling_soundtrack: AnimationPlayer = $"Falling Soundtrack"
+
+func _on_body_entered(_body: Node2D) -> void:
+	#plays hawk sounds
+	falling_soundtrack.play("Falling")
+	timer.start()
 	
-	
-	
-#func _on_timer_timeout() -> void:
+func _on_timer_timeout() -> void:
 	#reloads level
-	#get_tree().reload_current_scene()
+	if (falling == false):
+		falling = true
+		player._damage()
+		player.global_position = Vector2(-185, -260)
+		timer_2.start()
+
+
+func _on_timer_2_timeout() -> void:
+	falling = false
