5bbc50c89a501e12d0c399b256b712dc663daf7b
PeterBTech <solidsword@icloud.com>
2025-11-22 19:18:50 -0500

    sound fix

--- Scripts/counter.gd
+++ Scripts/counter.gd
@@ -12,9 +12,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
 
-
-func _on_pickups_child_exiting_tree(node: Node) -> void:
+func _on_pickups_2_child_exiting_tree(node: Node) -> void:
 	coins += 1
 	counter.text = "coins: " + str(coins) + "/5"
-	
